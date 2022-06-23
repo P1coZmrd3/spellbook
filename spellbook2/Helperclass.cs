939c209fd0df1633402efb86f57efedad5434ba8
Vlastnik <Vlastnik@DESKTOP-70EK70P>
2022-06-23 20:00:52 +0200

    Přidejte soubory projektu.

---
+++ spellbook2/Helperclass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellBook
+{
+    internal class HelperClass
+    {
+        public Random generator = new Random();
+
+        public string[] PoleZTextu(string adressSource)
+        {
+            string obsahTxt = File.ReadAllText(adressSource);
+            string[] PoleZSouboru = obsahTxt.Split(";");
+
+            return PoleZSouboru;
+        }
+
+        public string VygenerujSpellname(string[] poleForms, string[] poleTypes)
+        {
+            int randomName = generator.Next(23);
+            string spellName = $"{poleForms[randomName]} {poleTypes[randomName]}";
+
+            return spellName;
+        }
+
+        public int VygenerujCislo(int max)
+        {
+            int randomCislo = generator.Next(max);
+
+            return randomCislo;
+        }
+    }
+}
